ecf713ecf65032f2dc49f5b91fb9080dfe745ae9
AnastasiaBor <anast.bor@mail.ru>
2017-06-07 14:48:20 +0300

    remove old files

--- countriess.py
+++
@@ -1,5 +0,0 @@
-from pygal.maps.world import COUNTRIES
-from pygal_maps_world import i18n
-
-for country_code in sorted(COUNTRIES.keys()):#ключи сортируются в алфавитном порядке
-    print(country_code, COUNTRIES[country_code])
